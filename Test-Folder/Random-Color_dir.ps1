2082e9426f5effadba38df97ffa78ca2bbb87bc1
Erik Arnesen <earnesen@ymail.com>
2018-10-23 06:56:54 -0400

    More testing small code

---
+++ Test-Folder/Random-Color_dir.ps1
@@ -0,0 +1,7 @@
+﻿$r = Get-ChildItem 
+$t = ("red","blue",'yellow','green')
+foreach($f in $r){
+$y = Get-Random -Minimum 0 -Maximum 3
+Start-Sleep -Seconds $y
+Write-Host $f -ForegroundColor $t[$y]
+}
---
+++ Test-Folder/Run-mainmenu.ps1
@@ -0,0 +1,14 @@
+﻿
+$name = "_________ Menu System _________"
+$menu = Get-Content .\Menu.txt
+
+import-module .\show-mainmenu.psm1
+
+#& ((Split-Path $MyInvocation.InvocationName) + "\Show-MainMenu.ps1")
+#Create-Menu -Title 'Welcome to the Maintenance Center' -MenuItems 'Set Safety On/Off','EXIT',"Move all VM's to one host",'Reboot Empty host',"Balance all VM's per 'tag'",'Move and Reboot and Balance VM environment','VM/Host information','Exit' -TitleColor Red -LineColor Cyan -MenuItemColor Yellow
+Create-Menu -Title 'Welcome to the Maintenance Center' -MenuItems $menu -TitleColor Red -LineColor Cyan -MenuItemColor Yellow
+
+#& ((Split-Path $MyInvocation.InvocationName) + "\PrintName2.ps1")
+& ((Split-Path $MyInvocation.InvocationName) + "\PrintName.ps1") -printName $name
+
+& ((Split-Path $MyInvocation.InvocationName) + "\Show-DynamicMenuOfCurrentDirectory.ps1")
