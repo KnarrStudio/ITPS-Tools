542c65f501e8365e42a228bd723b27e72899cd5b
Erik JA <OgJAkFy8@users.noreply.github.com>
2018-10-24 06:50:55 -0400

    Updated the remarks

--- Test-Folder/Run-mainmenu.ps1
+++ Test-Folder/Run-mainmenu.ps1
@@ -1,14 +1,18 @@
-﻿
+# This script is only to demonstrate calling modules and scripts from another
+
 $name = "_________ Menu System _________"
 $menu = Get-Content .\Menu.txt
 
+# Imports the menu module
 import-module .\show-mainmenu.psm1
 
-#& ((Split-Path $MyInvocation.InvocationName) + "\Show-MainMenu.ps1")
-#Create-Menu -Title 'Welcome to the Maintenance Center' -MenuItems 'Set Safety On/Off','EXIT',"Move all VM's to one host",'Reboot Empty host',"Balance all VM's per 'tag'",'Move and Reboot and Balance VM environment','VM/Host information','Exit' -TitleColor Red -LineColor Cyan -MenuItemColor Yellow
+# Call the function in the module using the variable $menu, but could be "item1",'item2','item3' obviously, the latter does not scale well 
+# The next can only be done becuse create-menu is a function in the show-mainmenu module
 Create-Menu -Title 'Welcome to the Maintenance Center' -MenuItems $menu -TitleColor Red -LineColor Cyan -MenuItemColor Yellow
 
-#& ((Split-Path $MyInvocation.InvocationName) + "\PrintName2.ps1")
+# Demonstrates passing a switch to from one script to another. 
+# Using the split-path method, you have to have both scripts in the same folder.  
+# The $MyInvocation finds the path that the script is in and uses it 
 & ((Split-Path $MyInvocation.InvocationName) + "\PrintName.ps1") -printName $name
 
 & ((Split-Path $MyInvocation.InvocationName) + "\Show-DynamicMenuOfCurrentDirectory.ps1")
