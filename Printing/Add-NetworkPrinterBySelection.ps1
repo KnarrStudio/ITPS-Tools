90009ec859a97c2226947453db565dc60cb44591
Erik Arnesen <earnesen@ymail.com>
2018-11-08 07:06:14 -0500

    Update Printing/Add-NetworkPrinterBySelection.ps1
    Added notes

--- Printing/Add-NetworkPrinterBySelection.ps1
+++ Printing/Add-NetworkPrinterBySelection.ps1
@@ -1,3 +1,18 @@
+  <#
+      .SYNOPSIS
+      Retrieves all of the printers you are allowed to see on a print server that you designate.  Allows you to select it and adds the printer to your local workstation.
+
+      .PARAMETER Servername
+      Name of the print server you will be attaching to.
+
+      .EXAMPLE
+      Add-NetworkPrinterBySelection
+	
+      .NOTES
+      Place additional notes here.
+  #>
+
+
 $PrintServer = '\\Servername'
 
 $PrinterSelection = get-printer <#-ComputerName $PrintServer#> | select Name, DriverName,PortName | Out-GridView -PassThru
