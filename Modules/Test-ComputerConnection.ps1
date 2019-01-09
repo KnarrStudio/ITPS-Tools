1a4d2f79f19a242ab971097bae2d50c5481a9e76
Erik Arnesen <earnesen@ymail.com>
2019-01-09 06:45:23 -0500

    New file for testing a collection of workstations

---
+++ Modules/Test-ComputerConnection.ps1
@@ -0,0 +1,18 @@
+﻿$d = Import-Csv C:\Temp\SiteList.csv -Header Systems
+$GoodCount = $BadCount = 0
+$u = Get-Date -UFormat %Y-%m-%d
+$ReportFile = "C:\temp\$u-Report.txt"
+
+foreach($r in $d){
+
+$t = Test-Connection $r.Systems -Count 1 -Quiet
+if($t -ne 'True'){
+    $BadCount =+ 1
+
+    $r.Systems | Out-File $ReportFile -NoClobber -Append
+    }
+else{
+    $GoodCount += 1
+    }
+}
+ 
