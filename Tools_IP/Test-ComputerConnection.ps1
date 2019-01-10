02095917433a6992e8da9f03a639c8c3cd309ed8
Erik Arnesen <earnesen@ymail.com>
2019-01-09 20:27:20 -0500

    Moved file to different folder

---
+++ Tools_IP/Test-ComputerConnection.ps1
@@ -0,0 +1,18 @@
+﻿$WorkstationList = Import-Csv -Path $env:HOMEDRIVE\Temp\SiteList.csv -Header Name
+$GoodCount = $BadCount = 0
+$DateNow = Get-Date -UFormat %Y-%m-%d
+$ReportFile = ("$env:HOMEDRIVE\temp\{0}-Report.txt" -f $DateNow)
+
+foreach($OneWorkstation in $WorkstationList){
+
+   $t = Test-Connection -ComputerName $OneWorkstation.Name -Count 1 -Quiet
+   if($t -ne 'True'){
+      $BadCount =+ 1
+
+      $OneWorkstation.Name | Out-File -FilePath $ReportFile -NoClobber -Append
+   }
+   else{
+      $GoodCount += 1
+   }
+}
+ 
