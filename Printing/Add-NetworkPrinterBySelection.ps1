bc51f552dbb526d0a2a86093bed82ad1502995cd
Erik Arnesen <earnesen@ymail.com>
2019-02-20 22:04:26 -0500

    Update Add-NetworkPrinterBySelection.ps1

--- Printing/Add-NetworkPrinterBySelection.ps1
+++ Printing/Add-NetworkPrinterBySelection.ps1
@@ -1,19 +1,49 @@
-  <#
-      .SYNOPSIS
-      Retrieves all of the printers you are allowed to see on a print server that you designate.  Allows you to select it and adds the printer to your local workstation.
+﻿#Requires -version 3.0
 
-      .PARAMETER Servername
-      Name of the print server you will be attaching to.
+function Add-NetworkPrinter
+{
+   <#
+         .SYNOPSIS
+         Retrieves all of the printers you are allowed to see on a print server that you designate.  
+         Allows you to select it and adds the printer to your local workstation.
 
-      .EXAMPLE
-      Add-NetworkPrinterBySelection
-	
-      .NOTES
-      Place additional notes here.
-  #>
+         .PARAMETER Servername
+         Name of the print server you will be attaching to.
+
+         .EXAMPLE
+         Add-NetworkPrinterBySelection
+
+         .NOTES
+         Place additional notes here.
+   #>
 
 
-$PrintServer = '\\Servername'
+   
+   param
+   (
+      [Parameter(Mandatory,HelpMessage='Enter the printserver name')]
+      [String]$PrintServer
+   )
+   
+   if(!(Get-Module -Name PrintManagement))
+   {
+      Write-Verbose -Message 'Importing Print Management Module'
+      #Import-Module -Name PrintManagement
+   }
+   Write-Verbose -Message 'Print Management Module Imported'
+   
+   [CmdletBinding()]
+         
+   $PrinterSelection = get-printer -ComputerName $PrintServer | Select-Object -Property Name, DriverName,PortName | Out-GridView -PassThru
+   Write-Verbose -Message ('Printer Selected {0}' -f $PrinterSelection)
+   
+   $PrinterName = $PrinterSelection.name
+   Write-Verbose -Message ('Pritner Name {0}' -f $PrinterName)
+   
+   Add-Printer -ConnectionName "\\$PrintServer\$PrinterName"  -WhatIf
+   Write-Verbose -Message 'Printer Connected'
+   
+}
 
-$PrinterSelection = get-printer <#-ComputerName $PrintServer#> | select Name, DriverName,PortName | Out-GridView -PassThru
-Add-Printer -ConnectionName ('{0}\{1}' -f $PrintServer,$PrinterSelection.name) -whatif
+
+#Add-NetworkPrinter -PrintServer
