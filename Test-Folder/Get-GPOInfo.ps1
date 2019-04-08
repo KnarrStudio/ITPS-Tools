c291afb0d7db7c2371bd42d62a6261414ab75489
Erik Arnesen <earnesen@ymail.com>
2019-04-08 07:16:47 -0400

    Update Get-GPOInfo.ps1

--- Test-Folder/Get-GPOInfo.ps1
+++ Test-Folder/Get-GPOInfo.ps1
@@ -11,14 +11,15 @@ $Selection = Read-Host "Enter HTM or XML"
 switch ($Selection){
   'HTML' {
         # Get HTML report of all GPO's
-        Get-GPO -All | % {$_.GenerateReport('html') | Out-File "$($_.DisplayName).htm"}
+        Get-GPO -All | % {$_.GenerateReport('html') | Out-File "$env:USERPROFILE\Documents\GPOReports\$($_.DisplayName).htm"}
     }
   'HTM' {
-        Get-GPOReport -All -Domain "sales.contoso.com" -Server "DC1" -ReportType HTML -Path "C:\GPOReports\GPOReport1.html"
+        Get-GPOReport -All -Domain "sales.contoso.com" -Server "DC1" -ReportType HTML -Path "$env:USERPROFILE\Documents\GPOReports\GPOReport1.html"
+        #Get-GPOReport -All -Domain "sales.contoso.com" -Server "DC1" -ReportType HTML -Path "C:\GPOReports\GPOReport1.html"
     }
   'XML' {
         # Get XML Report of all GPO's
-        Get-GPO -All | % {$_.GenerateReport('xml') | Out-File "$($_.DisplayName).xml"}
+        Get-GPO -All | % {$_.GenerateReport('xml') | Out-File "$env:USERPROFILE\Documents\GPOReports\$($_.DisplayName).xml"}
     }
 
   Default {Write-Host 'Exit'}
