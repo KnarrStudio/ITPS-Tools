c99a7624860f870776b0035aaf43be694fa5361e
Erik Arnesen <earnesen@ymail.com>
2019-04-13 09:17:57 -0400

    Update Compare-Folders.ps1

--- Self-Help/Compare-Folders.ps1
+++ Self-Help/Compare-Folders.ps1
@@ -1,8 +1,26 @@
-﻿#requires -version 3.0
+﻿#Requires -version 5.0
+#quires -RunAsAdministrator
 
-#$FolderSource = "\\rsrcnfs08\OMC-CA_Org\MCAA\1606 (Info Technology)\_Staging"
+<#
+    .Synopsis
+    Short description
+    .DESCRIPTION
+    Long description
+    .EXAMPLE
+    Example of how to use this cmdlet
+    .EXAMPLE
+    Another example of how to use this cmdlet
+#>
+
+
+$FolderSource = "$env:USERPROFILE\Documents\GitHub\OgJAkFy8\ITPS-Tools\" 
+$FolderDest = "$env:USERPROFILE\Documents\GitHub\PS-Scripts\"
+#$FolderSource = '\\rsrcngmfs02\OMC-CA_Org\OMC-S\IT' 
+#$FolderDest = '\\rsrcnfs08\OMC-CA_Org\OMC-S\IT'
+
    
-function Compare-Folders {
+function Compare-Folders 
+{
   <#
       .SYNOPSIS
       Compare two folders for clean up
@@ -18,50 +36,79 @@ function Compare-Folders {
 
   [Cmdletbinding()]
   
-   Param(
-    [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+  Param(
+    [Parameter(Mandatory = $true,ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [Alias('Source','OldFolder')]
-    [string]$FolderSource = '\\rsrcngmfs02\OMC-CA_Org\OMC-S\IT',
-    #$F = 'C:\Temp'
-    
-    [Alias('Destination','Staging')]
-    [string]$FolderDest = '\\rsrcnfs08\OMC-CA_Org\OMC-S\IT'
+    [string]$FolderSource,
+        
+    [Parameter(Mandatory = $true)][Alias('Destination','Staging')]
+    [string]$FolderDest
   )
 
-  $FolderSourceSelected = Get-ChildItem -Path $FolderSource | Where-Object {$_.Name -in $(Get-ChildItem -Path $FolderDest).Name} | Select-Object -Property Name,FullName | Out-GridView -Title 'Select Folder to Compare' -OutputMode Single 
-  #$FolderDestSelected = Get-ChildItem $FolderDest  | select Name,FullName | Out-GridView -Title "Compare to '$($FolderSourceSelected.Name)'" -OutputMode Single 
-  $FolderDestSelected = Get-ChildItem -Path $FolderDest  | Where-Object Name -eq $($FolderSourceSelected.Name ) #select Name,FullName | Out-GridView -Title "Compare to '$($FolderSourceSelected.Name)'" -OutputMode Single 
-
-
-  Function Get-Recursed {
-    [CmdletBinding()]
-    Param(
-      [Alias('Source','OldFolder')]
-      [string]$FolderPath
+  function Get-FolderStats
+  {
+    [OutputType([int])]
+    Param
+    (
+      # Param1 help description
+      [Parameter(Mandatory,ValueFromPipelineByPropertyName,Position = 0)]
+      [String]$InputItem
     )
-    
-    $FullFolder = $FolderPath #.FullName
-    Get-ChildItem -Path $FullFolder -Recurse 
+  
+    Process {
+      Write-debug -Message $InputItem 
+      $InputItem |
+          Measure-Object -Sum -Property Length | 
+          Select-Object -Property count, Sum
+        }
   }
-
-  $FolderSourceRecurse =   Get-ChildItem -Path $FolderSourceSelected.FullName -Recurse
-  $FolderDest_InputObject =  Get-ChildItem -Path $FolderDestSelected.FullName -Recurse
-
-  $FolderSourceSize = $FolderSourceRecurse | Measure-Object -Sum -Property Length | Select-Object -Property count,Sum 
-  $FolderDestSize = $FolderDest_InputObject | Measure-Object -Sum -Property Length | Select-Object -Property count,Sum
-
-
-  Clear-Host
-  Compare-Object -ReferenceObject $FolderSourceRecurse -DifferenceObject $FolderDest_InputObject #| Wait-Process
+  function Get-Recursed 
+  {
+    [OutputType([int])]
+    Param
+    (
+      # Param1 help description
+      [Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
+      [Object]$FolderSelected
+    )
+    Process {
+      Get-ChildItem -Path $FolderSelected.FullName -Recurse
+    }
+  }
+  
+  $FolderSourceSelected = Get-ChildItem -Path $FolderSource |
+  Where-Object -FilterScript {
+    $_.Name -in $(Get-ChildItem -Path $FolderDest).Name
+  } |
+  Select-Object -Property Name, FullName |
+  Out-GridView -Title 'Select Folder to Compare' -OutputMode Single 
+  #$FolderDestSelected = Get-ChildItem $FolderDest  | select Name,FullName | Out-GridView -Title "Compare to '$($FolderSourceSelected.Name)'" -OutputMode Single 
+  
+  $FolderDestSelected = Get-ChildItem -Path $FolderDest  | Where-Object -Property Name -EQ -Value $($FolderSourceSelected.Name ) #select Name,FullName | Out-GridView -Title "Compare to '$($FolderSourceSelected.Name)'" -OutputMode Single 
+  Write-Debug -Message ('FolderSourceSelected  = {0}' -f $FolderSourceSelected)
+  Write-Debug -Message ('FolderDestSelected = {0}' -f $FolderDestSelected)
+  
+  $FolderSourceRecurse = Get-Recursed -FolderSelected $FolderSourceSelected
+  $FolderDest_InputObject = Get-Recursed -FolderSelected $FolderDestSelected
+  Write-Debug -Message ('FolderSourceRecurse = {0}' -f $FolderSourceRecurse)
+  Write-Debug -Message ('FolderDest_InputObject = {0}' -f $FolderDest_InputObject)
+  
+  <#
+      $FolderSourceSize = Get-FolderStats -InputItem $FolderSourceRecurse
+      $FolderDestSize = Get-FolderStats -InputItem $FolderDestSelected
+      Write-Debug "FolderSourceSize = $FolderSourceSize"
+      Write-Debug "FolderDestSize = $FolderDestSize"
+  #> 
+  
+  Compare-Object -ReferenceObject $FolderSourceRecurse -DifferenceObject $FolderDest_InputObject #| Out-Null
 
   Write-Verbose -Message ("'<=' only in {0} " -f $FolderSourceSelected.FullName) 
   Write-Verbose -Message ("'=>' only in {0} " -f $FolderDestSelected.FullName) 
-  $FolderSourceSize
-  $FolderDestSize
-
-  #Get-ChildItem -Directory -Recurse -Depth 2 | ForEach-Object{if($_.LastAccessTime -lt $(get-date).AddDays(-720)){$_ | select Parent,BaseName,CreationTime,Attributes | Export-Csv c:\temp\software.csv}} 
-
-  
-
+  #Write-Verbose -Message $FolderSourceSize
+  #Write-Verbose -Message $FolderDestSize
 }
+
+Clear-Host
+Compare-Folders -FolderSource $FolderSource  -FolderDest $FolderDest -Verbose
+
 
