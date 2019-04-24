b63250bc62d3ba58f0e3c568b16a9a693784cc03
Erik JA <OgJAkFy8@users.noreply.github.com>
2019-04-24 08:48:52 -0400

    Corrected a couple of small issues

--- Self-Help/Repair-FolderRedirection.ps1
+++ Self-Help/Repair-FolderRedirection.ps1
@@ -9,7 +9,6 @@ function Repair-FolderRedirection
       
       .EXAMPLE
       Repair-FolderRedirection
-
       .EXAMPLE
       Repair-FolderRedirection -RemotePath 'H:\_MyComputer'
   #>
@@ -22,7 +21,7 @@ function Repair-FolderRedirection
     [Parameter(Mandatory = $false,
         ValueFromPipelineByPropertyName,
     Position = 0)]
-    [string]$RemotePath = 'H:\_MyComputer'
+    [string]$RemotePath = "$env:HOMEDRIVE\_MyComputer"
   )
   
   Begin
@@ -35,45 +34,56 @@ function Repair-FolderRedirection
       'My Video'  = 'Videos'
       'Personal'  = 'Documents'
     }
+
     $Keys = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders', 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
-    $RemotePath = $Env:USERPROFILE
+    $LocalPath = $Env:USERPROFILE
+    $errorlog = "ErrorLog-$(Get-Date -UFormat %d%S).txt"
   }
   Process
   {
     foreach($FolderKey in $FolderList.keys)
     {
       $FolderName = $FolderList.Item($FolderKey)
-      $OldPath = ('{0}\{1}' -f $RemotePath, $FolderName)
+      $OldPath = ('{0}\{1}' -f $LocalPath, $FolderName)
       $NewPath = ('{0}\{1}' -f $RemotePath, $FolderName)
-
+      Write-Verbose -Message ('FolderName = {0}' -f $FolderName)
+      Write-Verbose -Message ('OldPath = {0}' -f $OldPath)
+      Write-Verbose -Message ('NewPath = {0}' -f $NewPath)
+            
       If(-Not(Test-Path -Path $NewPath ))
       {
-        New-Item -Path $NewPath -ItemType Directory -WhatIf
+        Write-Verbose -Message ('NewPath = {0}' -f $NewPath)
+        New-Item -Path $NewPath -ItemType Directory
       }
 
-      Copy-Item -Path $OldPath -Destination $RemotePath -Recurse -WhatIf
+      Write-Verbose -Message ('OldPath = {0}' -f $OldPath)
+      try
+      {
+        Copy-Item -Path $OldPath -Destination $RemotePath -Recurse -ErrorAction stop  # -ErrorAction SilentlyContinue
+      }
+      catch
+      {
+        $OldPath + $_.Exception.Message | Out-File -FilePath ('{0}\{1}' -f $RemotePath, $errorlog) -Append
+      }
       
       foreach($RegKey in $Keys)
       {
+        Write-Verbose -Message ('FolderKey = {0}' -f $FolderKey)
+        Write-Verbose -Message ('FolderName = {0}' -f $FolderName)
+
+        Write-Verbose -Message ('RegKey = {0}' -f $RegKey)
         Get-ItemProperty -Path $RegKey -Name $FolderKey
 
         #Test Path - Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders'
-        Write-Verbose -Message $FolderKey
-        Write-Verbose -Message $FolderName
-
-
+ 
         Set-ItemProperty -Path $RegKey -Name $FolderKey -Value $NewPath
       }
     }
   }
 }
-End
-{
 
-}
  
-Repair-FolderRedirection -RemotePath 'H:\_MyComputer'
-
+Repair-FolderRedirection -RemotePath 'H:\_MyComputer' -Verbose
 
 
 
