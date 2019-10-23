f518dbea31d1e9f2d26096f89f2cec2bec018514
Erik Arnesen <earnesen@ymail.com>
2019-10-22 20:02:27 -0400

    Update WorkstationPing.ps1

--- Tools-PowerPing/WorkstationPing.ps1
+++ Tools-PowerPing/WorkstationPing.ps1
@@ -1,31 +1,44 @@
 function Test-AdWorkstationConnections
 {
-  [CmdletBinding()]
-  param
-  (
-    [Switch]$Bombastic
+
+  [CmdletBinding(SupportsShouldProcess,ConfirmImpact = 'Low')]
+  param(
+
+    [Parameter(Mandatory=$false, Position=1)]
+    [String]
+    $ADSearchBase = 'OU=Clients-Desktop,OU=Computers,DC=Knarrstudio,DC=net',
+    
+    [Parameter(Mandatory=$false, Position=0)]
+    [String]
+    $PingReportFolder = '\\server\share\Reports\PingReport',
+
+        [Parameter(Mandatory=$false, Position=2)]
+    [string]
+    $OutputFileName = 'AdDesktop',
+    
+       [Switch]$Bombastic
   )
-  $ADSearchBase = 'OU=Clients-Desktop,OU=Computers,DC=Knarrstudio,DC=net'
-  $PingReportFolder = '\\server\share\Reports\PingReport'
+  
+  $i = 1
   $BadCount = 0
   $DateNow = Get-Date -UFormat %Y%m%d-%H%M
-  $ReportFile = (('{0}\{1}-AdDesktopReport.csv' -f $PingReportFolder, $DateNow))
-  $WorkstationSiteList = (('{0}\{1}-AdDesktopList.csv' -f $PingReportFolder, $DateNow))
-  $i = 1
+  $OutputFileName = ('{0}\{1}-{2}_Results.csv' -f $PingReportFolder, $DateNow,$OutputFileName)
+  $WorkstationSiteList = ('{0}\{1}-{2}_List.csv' -f $PingReportFolder, $DateNow,$OutputFileName)
+  
 
   if(!(Test-Path -Path $PingReportFolder))
   {
     New-Item -Path $PingReportFolder -ItemType Directory
   }
-
+  
   Get-ADComputer -filter * -SearchBase $ADSearchBase -Properties * |
   Select-Object -Property Name, LastLogonDate, Description |
   Sort-Object -Property LastLogonDate -Descending |
   Export-Csv -Path $WorkstationSiteList -NoTypeInformation
-
+  
   $WorkstationList = Import-Csv -Path $WorkstationSiteList -Header Name
   $TotalWorkstations = $WorkstationList.count -1
-
+  
   if($TotalWorkstations -gt 0)
   {
     foreach($OneWorkstation in $WorkstationList)
@@ -42,23 +55,23 @@ function Test-AdWorkstationConnections
           $WorkstationProperties = Get-ADComputer -Identity $WorkstationName -Properties * | Select-Object -Property Name, LastLogonDate, Description
           if($BadCount -eq 1)
           {
-            $WorkstationProperties | Export-Csv -Path $ReportFile -NoClobber -NoTypeInformation
+            $WorkstationProperties | Export-Csv -Path $OutputFileName -NoClobber -NoTypeInformation
           }
           else
           {
-            $WorkstationProperties | Export-Csv -Path $ReportFile -NoTypeInformation -Append
+            $WorkstationProperties | Export-Csv -Path $OutputFileName -NoTypeInformation -Append
           }
         }
       }
     }
   }
-
+  
   if ($Bombastic)
   {
     Write-Host ('Total workstations found in AD: {0}' -f $TotalWorkstations) -ForegroundColor Green
     Write-Host ('Total workstations not responding: {0}' -f $BadCount) -ForegroundColor Red
     Write-Host "This test was run by $env:USERNAME from $env:COMPUTERNAME"
-    Write-Host ('You can find the full report at: {0}' -f $ReportFile)
+    Write-Host ('You can find the full report at: {0}' -f $OutputFileName)
   }
 }
 
