6b321ab246a939d0b34fd0b2733484baab67f185
Erik Arnesen <earnesen@ymail.com>
2019-10-22 20:02:58 -0400

    Formatting

--- Tools-PowerPing/PrintersWorkstationsSwitches.psm1
+++ Tools-PowerPing/PrintersWorkstationsSwitches.psm1
@@ -95,32 +95,45 @@ function Test-PrinterStatus
 
 function Test-AdWorkstationConnections
 {
-  [CmdletBinding()]
-  param
-  (
-    [Switch]$Bombastic
+
+  [CmdletBinding(SupportsShouldProcess,ConfirmImpact = 'Low')]
+  param(
+
+    [Parameter(Mandatory=$false, Position=1)]
+    [String]
+    $ADSearchBase = 'OU=Clients-Desktop,OU=Computers,DC=Knarrstudio,DC=net',
+    
+    [Parameter(Mandatory=$false, Position=0)]
+    [String]
+    $PingReportFolder = '\\server\share\Reports\PingReport',
+
+        [Parameter(Mandatory=$false, Position=2)]
+    [string]
+    $OutputFileName = 'AdDesktop',
+    
+       [Switch]$Bombastic
   )
-  $ADSearchBase = 'OU=Clients-Desktop,OU=Computers,DC=Knarrstudio,DC=net'
-  $PingReportFolder = '\\server\share\Reports\PingReport'
+  
+  $i = 1
   $BadCount = 0
   $DateNow = Get-Date -UFormat %Y%m%d-%H%M
-  $ReportFile = (('{0}\{1}-AdDesktopReport.csv' -f $PingReportFolder, $DateNow))
-  $WorkstationSiteList = (('{0}\{1}-AdDesktopList.csv' -f $PingReportFolder, $DateNow))
-  $i = 1
+  $OutputFileName = ('{0}\{1}-{2}_Results.csv' -f $PingReportFolder, $DateNow,$OutputFileName)
+  $WorkstationSiteList = ('{0}\{1}-{2}_List.csv' -f $PingReportFolder, $DateNow,$OutputFileName)
+  
 
   if(!(Test-Path -Path $PingReportFolder))
   {
     New-Item -Path $PingReportFolder -ItemType Directory
   }
-
+  
   Get-ADComputer -filter * -SearchBase $ADSearchBase -Properties * |
   Select-Object -Property Name, LastLogonDate, Description |
   Sort-Object -Property LastLogonDate -Descending |
   Export-Csv -Path $WorkstationSiteList -NoTypeInformation
-
+  
   $WorkstationList = Import-Csv -Path $WorkstationSiteList -Header Name
   $TotalWorkstations = $WorkstationList.count -1
-
+  
   if($TotalWorkstations -gt 0)
   {
     foreach($OneWorkstation in $WorkstationList)
@@ -137,37 +150,65 @@ function Test-AdWorkstationConnections
           $WorkstationProperties = Get-ADComputer -Identity $WorkstationName -Properties * | Select-Object -Property Name, LastLogonDate, Description
           if($BadCount -eq 1)
           {
-            $WorkstationProperties | Export-Csv -Path $ReportFile -NoClobber -NoTypeInformation
+            $WorkstationProperties | Export-Csv -Path $OutputFileName -NoClobber -NoTypeInformation
           }
           else
           {
-            $WorkstationProperties | Export-Csv -Path $ReportFile -NoTypeInformation -Append
+            $WorkstationProperties | Export-Csv -Path $OutputFileName -NoTypeInformation -Append
           }
         }
       }
     }
   }
-
+  
   if ($Bombastic)
   {
     Write-Host ('Total workstations found in AD: {0}' -f $TotalWorkstations) -ForegroundColor Green
     Write-Host ('Total workstations not responding: {0}' -f $BadCount) -ForegroundColor Red
     Write-Host "This test was run by $env:USERNAME from $env:COMPUTERNAME"
-    Write-Host ('You can find the full report at: {0}' -f $ReportFile)
+    Write-Host ('You can find the full report at: {0}' -f $OutputFileName)
   }
 }
 
-function Test-FiberSatillite
+
+function Test-FiberSatellite
 {
+  <#
+      .SYNOPSIS
+      "Pings" a group of sites or servers and gives a response in laymans terms.
+
+      .DESCRIPTION
+      This started due to our need to find out if we were running on Fiber or not.  There are some default off island sites that it will test, but you can pass them if you only want to check one or two sites.
+
+      .PARAMETER Sites
+      A single or list of sites or servers that you want to test against.
+
+      .PARAMETER OneLineOutput
+      Provides a single output line for those who just need answers.
+
+      .EXAMPLE
+      Test-FiberSatellite -Sites Value -OneLineOutput
+      Tests the Value and displays the output as a single line of text
+
+      .NOTES
+      Place additional notes here.
+
+      .LINK
+      https://github.com/KnarrStudio/ITPS-Tools/wiki
+
+      .OUTPUTS
+      To console or screen at this time.
+  #>
+
   param
   (
     [Parameter(Position = 0)]
-    [Object[]]
-    $Sites = ('www.google.com', 'www.bing.com', 'www.cnn.com', 'www.facebook.com', 'www.yahoo.com')
+    [Object[]] $Sites = ('LocalServer', 'localhost', 'www.google.com', 'www.bing.com', 'www.wolframalpha.com', 'www.yahoo.com'),
+    [Switch]$OneLineOutput
   )
   
-  $RttTotal = 0
-  $TotalSites = $Sites.Count
+  $RttTotal = $NotRight = 0
+  $TotalResponses = $TotalSites = $Sites.Count
   function Test-Verbose 
   {
     [Management.Automation.ActionPreference]::SilentlyContinue -ne $VerbosePreference
@@ -176,16 +217,31 @@ function Test-FiberSatillite
   ForEach ($Site in $Sites)  
   {
     $PingReply = Test-NetConnection -ComputerName $Site 
-    $RTT = $PingReply.PingReplyDetails.RoundtripTime
-    $RttTotal = $RttTotal + $RTT
+    if($PingReply.PingSucceeded -eq $true)
+    {
+      $RTT = $PingReply.PingReplyDetails.RoundtripTime
+      $RttTotal = $RttTotal + $RTT
     
-    Write-Verbose -Message ('{0} - RoundTripTime is {1} ms.' -f $PingReply.Computername, $RTT)
+      if($RTT -eq 0)
+      {
+        $TotalResponses = $TotalResponses - 1
+        $NotRight = $NotRight + 1
+      }
+      Write-Verbose -Message ('{0} - RoundTripTime is {1} ms.' -f $PingReply.Computername, $RTT)
+    }
+    Else
+    {
+      $TotalResponses = $TotalResponses - 1
+    }
   }
 
-  $RTT = $RttTotal/$TotalSites
-    
-  if(Test-Verbose)
+  $RTT = $RttTotal/$TotalResponses
+  $TimeStamp = Get-Date -Format 'dd-MMM-yyyy HH:mm'
+  if(-not $OneLineOutput)
   {
+    Write-Host ('The Ping-O-Matic Fiber Tester!') -BackgroundColor DarkYellow -ForegroundColor White
+    <#    if(Test-Verbose)
+    {#>
     if($RTT -gt 380)
     {
       Write-Host('Although not always the case this could indicate that you are on the Satellite backup circuit.') -BackgroundColor Red -ForegroundColor White
@@ -194,11 +250,20 @@ function Test-FiberSatillite
     {
       Write-Host ('Although not always the case this could indicate that you are on the Puerto Rico backup circuit.') -BackgroundColor Yellow -ForegroundColor White
     }
-    ElseIf($RTT -gt 0)
+    ElseIf($RTT -gt 1)
     {
       Write-Host ('Round Trip Time is GOOD!') -BackgroundColor Green -ForegroundColor White
     }
+    # }
   }
+  Write-Host ('{1} - {3} tested {0} off island computers and {2} responded. The average response time:' -f $TotalSites, $TimeStamp, $TotalResponses, $env:USERNAME) -ForegroundColor DarkYellow -NoNewline
+  Write-Output -InputObject (' {0} ms' -f [int]$RTT)
+
+  if($NotRight -gt 0)
+  {
+    Write-Output -InputObject ('{0} Responded with 0 ms' -f $NotRight)
+  }
+
   <#  Write-Output -InputObject ('Average RTT is {0} ms.' -f [int]$RTT)
       if ($RTT -lt 380){
   Start-Process "${env:ProgramFiles(x86)}\Notepad++\notepad++.exe" }#>
